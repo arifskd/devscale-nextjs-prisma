2e92ae6ceca226c1f7961669160e4208759b56b9
Reza Rinaldi <rez4rinaldi@gmail.com>
2024-04-29 21:03:33 +0700

    chore: adding desc aws s3

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "@/styles/globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Template Next.js & Prisma | Devscale ID",
-  description: "Template Project Next.js & Prisma for Devscale ID",
+  title: "Template Next.js, Prisma, and AWS S3 | Devscale ID",
+  description: "Template Project Next.js, Prisma, and AWS S3 for Devscale ID",
   publishedTime: "06-03-2024",
   authors: ["Devscale ID Student"],
   icons: {
